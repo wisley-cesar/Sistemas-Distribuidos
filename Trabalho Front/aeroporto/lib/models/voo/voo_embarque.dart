a784246f7d01f97b679915a762db2816a7e2e279
Wisley César <wisleycesar32@gmail.com>
2025-06-01 15:37:48 -0300

    FEAT: Criando o voo

---
+++ Trabalho Front/aeroporto/lib/models/voo/voo_embarque.dart
@@ -0,0 +1,47 @@
+import 'package:aeroporto/models/portao/portao_embarque.dart';
+import 'package:aeroporto/models/voo/status_voo.dart';
+
+class VooEmbarque {
+  final String id;
+  final int numeroVoo;
+  final String origem;
+  final String destino;
+  final String dataHoraPartida;
+  final PortaoEmbarque portaoEmbarqueId;
+  final StatusVoo statusVoo;
+  final bool ativo;
+
+  VooEmbarque({
+    required this.id,
+    required this.numeroVoo,
+    required this.origem,
+    required this.destino,
+    required this.dataHoraPartida,
+    required this.portaoEmbarqueId,
+    required this.statusVoo,
+    required this.ativo,
+  });
+
+  VooEmbarque.fromJson(Map<String, dynamic> json)
+    : id = json['id'],
+      numeroVoo = json['numeroVoo'],
+      origem = json['origem'],
+      destino = json['destino'],
+      dataHoraPartida = json['dataHoraPartida'],
+      portaoEmbarqueId = PortaoEmbarque.fromJson(json['portaoEmbarqueId']),
+      statusVoo = StatusVoo.fromJson(json['statusVoo']),
+      ativo = json['ativo'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'numeroVoo': numeroVoo,
+      'origem': origem,
+      'destino': destino,
+      'dataHoraPartida': dataHoraPartida,
+      'portaoEmbarqueId': portaoEmbarqueId.toJson(),
+      'statusVoo': statusVoo.toJson(),
+      'ativo': ativo,
+    };
+  }
+}
