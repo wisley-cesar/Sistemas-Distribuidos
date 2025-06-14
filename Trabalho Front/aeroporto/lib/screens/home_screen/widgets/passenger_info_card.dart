853e8567c8974773eb4d825c53817f0d917534e4
Wisley César <wisleycesar32@gmail.com>
2025-06-14 16:52:23 -0300

    Arrumando o nome do passageiro

--- Trabalho Front/aeroporto/lib/screens/home_screen/widgets/passenger_info_card.dart
+++ Trabalho Front/aeroporto/lib/screens/home_screen/widgets/passenger_info_card.dart
@@ -16,7 +16,18 @@ class PassengerInfoCard extends StatelessWidget {
 
   String _capitalizeFirstLetter(String text) {
     if (text.isEmpty) return text;
-    return text[0].toUpperCase() + text.substring(1).toLowerCase();
+
+    // Divide o texto em palavras
+    final words = text.split(' ');
+
+    // Capitaliza cada palavra
+    final capitalizedWords = words.map((word) {
+      if (word.isEmpty) return word;
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    });
+
+    // Junta as palavras novamente
+    return capitalizedWords.join(' ');
   }
 
   @override
